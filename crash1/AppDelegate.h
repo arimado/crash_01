cdd42d059d92305cd9c1350fdfad2e693e89c69c
Jeremy Arimado <jeremy.arimado@wearesocial.net>
2013-10-14 10:22:43 +1100

    Initial Commit

---
+++ crash1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  crash1
+//
+//  Created by Jeremy Arimado on 14/10/13.
+//  Copyright (c) 2013 Jeremy Arimado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ crash1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  crash1
+//
+//  Created by Jeremy Arimado on 14/10/13.
+//  Copyright (c) 2013 Jeremy Arimado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
